545c66c369fadfa3b2fdcbfae382e9e06ddce39e
omjadhav18 <om427880@gmail.com>
2025-04-24 21:13:02 +0530

    Update axiosall.js

--- src/utils/axiosall.js
+++ src/utils/axiosall.js
@@ -5,7 +5,7 @@ const access_token = Cookies.get("access_token")
 const refresh_token = Cookies.get("refresh_token")
 
 const apiInstanceAuth = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/',
+    baseURL: 'https://omjadhav.pythonanywhere.com/api/v1/',
     timeout: 10000,  // 100 seconds, typically 10 seconds is sufficient
     headers: {
         'Content-Type': 'application/json',
